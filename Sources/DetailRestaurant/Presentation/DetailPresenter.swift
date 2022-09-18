0214db32cd42335815125aeabaa98cb2740d0b71
Maitri Vira <maitrivira2@gmail.com>
2022-09-18 14:50:46 +0700

    edit detail presenter

---
+++ Sources/DetailRestaurant/Presentation/DetailPresenter.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maitri Vira on 18/09/22.
+//
+
+import SwiftUI
+import RxSwift
+import Core
+
+public class DetailPresenter<DetailRestoUseCase: UseCase, FavUseCase: UseCase>: ObservableObject
+where
+    DetailRestoUseCase.Request == String,
+    DetailRestoUseCase.Response == DetailRestaurantDomainModel,
+    FavUseCase.Request == Any,
+    FavUseCase.Response == [DetailRestaurantDomainModel]
+{
+    
+    private let disposeBag = DisposeBag()
+    
+    private let _detailUseCase: DetailRestoUseCase
+    private let _favUseCase: FavUseCase
+    
+    @Published public var list: [DetailRestaurantDomainModel] = []
+    @Published public var errorMessage: String = ""
+    @Published public var isLoading: Bool = false
+    @Published public var isError: Bool = false
+    @Published public var isSaved: Bool = false
+    
+    public init(detailUseCase: DetailRestoUseCase, favUseCase: FavUseCase) {
+        _detailUseCase = detailUseCase
+        _favUseCase = favUseCase
+    }
+    
+    public func getList(request: Request?) {
+        isLoading = true
+        _useCase.execute(request: request)
+            .observe(on: MainScheduler.instance)
+            .subscribe { result in
+                self.list = result
+            } onError: { error in
+                self.errorMessage = error.localizedDescription
+                self.isError = true
+                self.isLoading = false
+            } onCompleted: {
+                self.isLoading = false
+            }.disposed(by: disposeBag)
+    }
+    
+    func containsId(of id: String) -> Bool {
+        let data = list.contains { $0.id == id }
+        return data
+    }
+}
