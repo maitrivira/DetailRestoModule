c0999f6fc315574dcd421faaaf21f634e68bf16c
Maitri Vira <maitrivira2@gmail.com>
2022-09-19 00:01:10 +0700

    edit update fav repo

--- Sources/DetailRestaurant/Data/UpdateFavoriteRepository.swift
+++ Sources/DetailRestaurant/Data/UpdateFavoriteRepository.swift
@@ -33,6 +33,6 @@ where
         }
         
         public func execute(request: DetailRestaurantDomainModel?) -> Observable<Bool> {
-            return _localeDataSource.addRestaurant(entities: _mapper.transformDomainToEntity(domain: request ?? emptyDetail))
+            return _localeDataSource.addRestaurant(entities: request)
         }
 }
