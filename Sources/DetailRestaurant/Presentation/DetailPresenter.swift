d093f74c9bac53baf1b98280d3562012ab8ed49a
Maitri Vira <maitrivira2@gmail.com>
2022-10-15 18:24:03 +0700

    add list favorite, delete getfavoriterepository

--- Sources/DetailRestaurant/Presentation/DetailPresenter.swift
+++ Sources/DetailRestaurant/Presentation/DetailPresenter.swift
@@ -9,25 +9,26 @@ import SwiftUI
 import RxSwift
 import Core
 
-public class DetailPresenter<DetailRestoUseCase: UseCase, FavUseCase: UseCase>: ObservableObject
+public class DetailPresenter<DetailRestoUseCase: UseCase, UpdateFavUseCase: UseCase, AllDetail: UseCase>: ObservableObject
 where
     DetailRestoUseCase.Request == String,
     DetailRestoUseCase.Response == DetailRestaurantDomainModel,
-    FavUseCase.Request == DetailRestaurantDomainModel,
-    FavUseCase.Response == Bool
+    UpdateFavUseCase.Request == DetailRestaurantDomainModel,
+    UpdateFavUseCase.Response == Bool
 {
     
     private let disposeBag = DisposeBag()
     private let _detailUseCase: DetailRestoUseCase
-    private let _favUseCase: FavUseCase
+    private let _favUseCase: UpdateFavUseCase
     
+    @Published public var lists: [DetailRestaurantDomainModel] = []
     @Published public var list: DetailRestaurantDomainModel?
     @Published public var errorMessage: String = ""
     @Published public var isLoading: Bool = false
     @Published public var isError: Bool = false
     @Published public var isSaved: Bool = false
     
-    public init(detailUseCase: DetailRestoUseCase, favUseCase: FavUseCase) {
+    public init(detailUseCase: DetailRestoUseCase, favUseCase: UpdateFavUseCase) {
         _detailUseCase = detailUseCase
         _favUseCase = favUseCase
     }
@@ -49,8 +50,9 @@ where
     }
     
     // update favourite
-    public func updateFavouriteList(request: FavUseCase.Request) {
+    public func updateFavouriteList(request: UpdateFavUseCase.Request) {
         isLoading = true
+        favorite(data: request)
         _favUseCase.execute(request: request)
             .observe(on: MainScheduler.instance)
             .subscribe { result in
@@ -62,5 +64,13 @@ where
             } onCompleted: {
                 self.isLoading = false
             }.disposed(by: disposeBag)
+    }
+    
+    private func favorite(data: UpdateFavUseCase.Request) {
+        let data = lists.filter { $0.id == data.id}
+        if data != nil {
+            lists.append(data)
+            print("data berhasil di simpan")
+        }
     }
 }
