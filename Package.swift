f026a55171af22b8a14ca19c0976e3ddcae5161b
Maitri Vira <maitrivira2@gmail.com>
2022-11-12 21:37:14 +0700

    add alamofire

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/maitrivira/CoreModule.git", from: "1.0.0")
+        .package(url: "https://github.com/maitrivira/CoreModule.git", from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +24,8 @@ let package = Package(
         .target(
             name: "DetailRestaurant",
             dependencies: [
-                .product(name: "Core", package: "CoreModule")
+                .product(name: "Core", package: "CoreModule"),
+                "Alamofire",
             ]),
         .testTarget(
             name: "DetailRestaurantTests",
